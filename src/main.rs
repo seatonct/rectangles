d1a5c0839fccd39c0535fc1265d30a5ece6ae467
Charles Seaton <seatonct@gmail.com>
2024-03-16 16:15:06 -0500

    completed main program

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(&rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
